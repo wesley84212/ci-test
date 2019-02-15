ec8c84fe406e723aeec0bc5e85544aaa0cdbee8e
wesley84212 <wesley84212@gmail.com>
2019-02-16 02:58:05 +0800

    add CRU database function delete not yet

--- application/models/Add_report_model.php
+++ application/models/Add_report_model.php
@@ -6,11 +6,61 @@ class Add_report_model extends CI_Model
     public function get_report_list($input)
     {
         $this->load->database();
-        $this->db->select('*');
+        $this->db->select('name');
         $this->db->from('report');
-        $this->db->where('user', $input);
+        $this->db->where('owner', $input);
         $this->db->where('active', '1');
         $query = $this->db->get();
-        return $query->result();
+        $array_list =[];
+        foreach ($query->result() as $row){
+            array_push($array_list,$row->name);
+        }
+        return $array_list;
     }
+
+    public function get_report_detail($input)
+    {
+        $this->load->database();
+        $this->db->select('content');
+        $this->db->from('report');
+        $this->db->where('owner',$input['owner']);
+        $this->db->where('name', $input['report_name']);
+        $this->db->where('active', '1');
+        $query = $this->db->get();
+        
+        foreach ($query->result() as $row){
+            echo $row->content;
+        }
+    }
+
+    public function update_report($input)
+    {
+        $this->load->database();       
+        $this->db->where('owner',$input['owner']);
+        $this->db->where('name', $input['report_name']);
+        unset($input['owner']);
+        unset($input['report_name']);
+        $this->db->trans_start();
+        $this->db->update('report',$input);
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === false) {
+            echo 'error';
+        } else {
+            echo 'success';
+        }
+    }
+
+    public function insert_report ($input)
+    {
+        $this->load->database();
+        $this->db->trans_start();
+            $this->db->insert('report', $input);
+            $this->db->trans_complete();
+            if ($this->db->trans_status() === false) {
+                echo 'error';
+            } else {
+                echo 'success';
+            }
+    }
+        
 }
--- application/views/login_page.php
+++ application/views/login_page.php
@@ -1,5 +1,5 @@
 <style>
-#logout {
+#info{
     float:right;
 }
 </style>
@@ -17,6 +17,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 <?php
+$this->jquery_loader->js_loader('add_report');
 $this->jquery_loader->js_loader('logout');
 $this->jquery_loader->css_loader('login_page');
 ?>
@@ -24,16 +25,54 @@ $this->jquery_loader->css_loader('login_page');
 
 <body>
    <div id="banner">
-       <div>登入成功 歡迎<span id="user"><?php echo $this->session->username ?><span></div>
-       <select id="get_report_list"><option>wesley_1</option></select>
-    <button type="submit" class="btn btn-primary" id="logout">登出</button>
-
+       <div id="info">歡迎<span id="user"><?php echo $this->session->username ?></span>
+       <button type="submit" class="btn btn-primary" id="logout">登出</button>
+    </div>
+       <select id="report_list">
+       <option>請選擇</option>
+       <?php 
+        $html='';
+           foreach($field as $value){
+            echo '<option>'.$value.'</option>';
+           }
+       ?>
+        </select>
 </div>
 
 <div id="report_detail">
 <textarea id="input_text" rows="4" cols="50"></textarea>
 </div>
-<button type="submit" class="btn btn-primary" id="save">存檔</button>
+<button type="submit" class="btn btn-primary" id="save" data-toggle="modal" data-target="#inset_report">新增</button>
+<button type="submit" class="btn btn-primary" id="update">修改</button>
+<button type="submit" class="btn btn-primary" id="delete">刪除</button>
+
+<!--inset report Modal -->
+<div class="modal fade" id="inset_report" taindex="-1" role="dialog" aria-labelledby="inset_report_label" aria-hidden="true">
+<div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="inset_report_label">新增</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                            <input tabindex="2" id="report_name" name="name" type="text" placeholder="請輸入名稱" class="form-control input-md" value="" required="">
+                    </div>
+                    <div class="form-group">
+                        請輸入內容
+                    <textarea id="report_text" rows="4" cols="50"></textarea>
+                    </div>
+                    <div class="form-group" id="reg_error">
+                    </div>
+                </div>
+                <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary" id="insert">送出</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 
 </html>
