68137e9ec905b42a0598c3fb2901164b4829f86c
wesley <wesley84212@gmail.com>
2019-02-18 09:28:42 +0800

    change url to base_url

--- application/views/sueruper_page.php
+++ application/views/sueruper_page.php
@@ -11,11 +11,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="http://127.0.0.1/ci-test/packages/jquery-ui/1.11.2/jquery-ui.min.css">
-    <link rel="stylesheet" href="http://127.0.0.1/ci-test/packages/jtable/2.4.0/themes/lightcolor/gray/jtable.min.css">
-    <script src="http://127.0.0.1/ci-test/packages/jquery-ui/1.11.2/jquery-ui.min.js"></script>
-    <script src="http://127.0.0.1/ci-test/packages/jtable/2.4.0/jquery.jtable.min.js"></script>
-    <script src="http://127.0.0.1/ci-test/packages/jtable/2.4.0/jtable.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/packages/jquery-ui/1.11.2/jquery-ui.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/packages/jtable/2.4.0/themes/lightcolor/gray/jtable.min.css">
+    <script src="<?php echo base_url(); ?>/packages/jquery-ui/1.11.2/jquery-ui.min.js"></script>
+    <script src="<?php echo base_url(); ?>/packages/jtable/2.4.0/jquery.jtable.min.js"></script>
+    <script src="<?php echo base_url(); ?>/packages/jtable/2.4.0/jtable.js"></script>
 <?php
 $this->jquery_loader->js_loader('logout');
 $this->jquery_loader->css_loader('login_page');
